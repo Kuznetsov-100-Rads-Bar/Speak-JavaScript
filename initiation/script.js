0829f4e46366a2aa2f5dbeb47e39dada0a5193d6
Kuznetsov-100-Rads-Bar <ledrifterdepripyat@gmail.com>
2022-05-20 14:16:15 +0200

    Creation dossier initialisation et mise à jour de ses fichiers

---
+++ initiation/script.js
@@ -0,0 +1,6 @@
+console.log("connecté !");
+// Sélection de l'élément à partir de son id
+// Accès à la propriété color
+// Nouvelle valeur -> modifier cette valeur en utilisant le signe =
+const titre = document.getElementById("titre").style.color = "green ";
+console.log(titre);
