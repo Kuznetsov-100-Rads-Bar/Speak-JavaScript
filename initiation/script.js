9f43019c3a33bb565ceb70ace271539a56b3684b
Kuznetsov-100-Rads-Bar <ledrifterdepripyat@gmail.com>
2022-05-20 20:20:48 +0200

    Mise à jour des fichiers css javascript et html

--- initiation/script.js
+++ initiation/script.js
@@ -2,5 +2,56 @@ console.log("connecté !");
 // Sélection de l'élément à partir de son id
 // Accès à la propriété color
 // Nouvelle valeur -> modifier cette valeur en utilisant le signe =
-const titre = document.getElementById("titre").style.color = "green ";
+let titre = document.getElementById("titre");
+titre.style.color = "green ";
+
 console.log(titre);
+
+
+/************************/
+/* Créer un mini jeu */
+/************************/
+
+// le bouton pour rejouer
+const replay = document.getElementById('replay');
+// console.log(replay);
+
+// Je sélectionne et je stocke
+const btnRed = document.getElementById('btn-red');
+console.log(btnRed, "bouton rouge");
+const btnBlue = document.getElementById('btn-blue');
+// console.log(btnBlue); 
+let compteur = 0;
+// console.log(compteur, "compteur au démarrage"); 
+
+// Je soumets le cercle rouge à une action
+btnRed.addEventListener('click', function () {
+    // console.log('cliqué'); 
+    add();
+});
+
+// Je soumets le cercle bleu à une action
+btnBlue.addEventListener('click', function () {
+    //console.log('cercle bleu cliqué'); 
+    add();
+});
+
+function add() {
+    compteur = compteur + 1;
+    console.log(compteur, "compteur après incrémentation");
+    titre.innerText = compteur;
+};
+
+
+setTimeout(function () {
+    // Je supprime les cercles
+    btnRed.remove();
+    btnBlue.remove();
+}, 10000);
+
+
+replay.addEventListener('click', function () {
+    console.log('cliqué');
+    location.reload();
+});
+
